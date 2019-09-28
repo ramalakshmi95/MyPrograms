1c8dcb0aa49d1349c8f05ee89e09b78074b72a03
ramalakshmi94 <jayashrishankaran04@gmail.com>
2019-09-28 22:15:34 +0530

    test

--- MyPrograms/src/main/java/folder/FactorialWithoutLoop.java
+++ MyPrograms/src/main/java/folder/FactorialWithoutLoop.java
@@ -6,6 +6,7 @@ public class FactorialWithoutLoop
 	{
 		int ans=1;
 		factorial(5,ans);
+		System.out.println(fact(5));
 	}
 	public static void factorial(int n,int ans)
 	{
@@ -17,5 +18,12 @@ public class FactorialWithoutLoop
 				factorial(n-1,ans);
 			}
 		
+	}
+	
+	public static int fact(int n)
+	{
+		if(n==1)
+			return 1;
+		return n*(fact(n-1));
 	}
 }
